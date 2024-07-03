bec17da8a3e5a0a6b26cb23b7891441ce353f51b
Maryam Qasim <maryam@DESKTOP-6B29GKT.>
2024-07-03 11:12:15 +0300

    dom-practice-starter

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
 // Query Selectors
+const sun = document.querySelector('.fa-sun');
+const moon = document.querySelector('.fa-moon');
+
+const switchMood = (event) => {
+    x = event.target.classList;
+    // console.log(x[1]);
+    body = document.body;
+    if (x[1] == 'fa-moon') {
+        body.classList.add('darkMood');
+        body.classList.remove('lightMood');
+    } 
+    else if (x[1] == 'fa-sun') {
+        body.classList.add('lightMood');
+        body.classList.remove('darkMood');
+    }
+};
+
+sun.addEventListener('click', switchMood);
+moon.addEventListener('click', switchMood);
